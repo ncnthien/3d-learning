195983f09b0e5c1a77cec55221d4a23968fcba4a
ncnthien <nhatthien185@gmail.com>
2024-01-06 22:10:51 +0700

    main: add new niceroom.glb

--- src/main4.ts
+++ src/main4.ts
@@ -6,15 +6,18 @@ const canvas = document.getElementById("c4")!;
 const renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(40, 2, 0.1, 1000);
-const light = new THREE.PointLight(0xffffff, 500);
+const light = new THREE.PointLight(0xffffff, 50);
+const secondLight = new THREE.PointLight(0xffffff, 50);
 const gltfLoader = new GLTFLoader();
 const controls = new OrbitControls(camera, renderer.domElement);
 
 controls.autoRotate = true;
 controls.target.set(0, 0, 0);
-light.position.set(0, 0, 0);
+light.position.set(0, 5, 0);
+secondLight.position.set(-5, 5, -5);
 camera.position.set(0, 2, 0);
 scene.add(light);
+scene.add(secondLight);
 scene.add(camera);
 
 gltfLoader.load(
